e3f6d0931106f9c9fefccf735f283dd66a0429e3
davidliu <david-khala@hotmail.com>
2018-09-18 15:48:31 +0800

    more details

--- interface.go
+++ interface.go
@@ -6,9 +6,9 @@ import (
 
 type PayerInterface interface {
 	//TODO: is callback designed needed?
-	TokenGetter(auth MemberAuth)
-	Proposal(auth ClinicAuth)
-	Modifier(auth ClinicAuth)
+	TokenGetter(auth MemberAuth) (tokenVerify, tokenPay string)
+	Proposal(auth ClinicAuth) (feeForm string)
+	Modifier(auth ClinicAuth, filledFeeForm string) (extraFee string)
 	TokenRefund(auth ClinicAuth)
 	Settlement(auth PayerAuth)
 }
@@ -18,4 +18,15 @@ type PayerAuth func(...string) bool
 type PayerChainCode struct {
 	golang.CommonChaincode
 	PayerInterface
+	ClinicAuth ClinicAuth
+	MemberAuth MemberAuth
+	PayerAuth  PayerAuth
 }
+type VisitData struct {
+	TokenVerify    string //provided by QRCode
+	TokenPay       string //provided by QRCode
+	ClinicID       string //provided by clinic
+	Doctor         Doctor //provided by clinic
+	MedicalNetwork string //provided by clinic
+}
+type Doctor string //doctor code
