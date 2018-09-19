6eccffbea1819392d77f5c7d1b38d66dd2a55d03
davidliu <david-khala@hotmail.com>
2018-09-19 17:02:40 +0800

    Add tokenData struct

---
+++ struct.go
@@ -0,0 +1,37 @@
+package go_chaincode_common
+
+import (
+	. "github.com/davidkhala/goutils"
+	"strconv"
+)
+
+type TokenData struct {
+	Owner     string
+	OwnerType OwnerType
+	TokenType TokenType
+}
+type OwnerType byte
+type TokenType byte
+
+func (t OwnerType) ToString() string {
+	switch t {
+	case 1:
+		return "member"
+	case 2:
+		return "network"
+	case 3:
+		return "insurance"
+	}
+	PanicString("invalid ownerType" + strconv.Itoa(int(t)))
+	return ""
+}
+func (t TokenType) ToString() string {
+	switch t {
+	case 1:
+		return "verify"
+	case 2:
+		return "pay"
+	}
+	PanicString("invalid tokenType" + strconv.Itoa(int(t)))
+	return ""
+}
