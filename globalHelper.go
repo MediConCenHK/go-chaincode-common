b6f6b90d7694e2fdc1db7f01123c4eaa067b5f16
davidliu <david-khala@hotmail.com>
2018-09-21 15:39:41 +0800

    remove debugger

--- globalHelper.go
+++ globalHelper.go
@@ -9,7 +9,6 @@ func PutTokenGlobal(t CommonChaincode, token string, tokenData TokenData) {
 	var args = ArgsBuilder("put")
 	var client = NewClientIdentity(t.CCAPI)
 	tokenData.Client = client
-	t.Logger.Info(string(ToJson(client)))
 	args.AppendArg("token")
 	args.AppendArg(token)
 	args.AppendArg(string(ToJson(tokenData)))
